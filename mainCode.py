9df8aecf3cca4fd98327329fb49ee3b8d0fa37fd
ED CARLOS DA SILVA PEREIRA <63868640+Goghed@users.noreply.github.com>
2022-03-22 13:58:53 -0300

    update v1.0.1

--- mainCode.py
+++ mainCode.py
@@ -1,13 +1,29 @@
 from deepface import DeepFace
 import cv2
 
-imagem1 = cv2.imread("Imagens Escolhidas/teste.jpg")
-imagem2 = cv2.imread("Imagens Escolhidas/teste2.jpg")
+#imagem1 = cv2.imread("Imagens Escolhidas/teste.jpg")
+#imagem2 = cv2.imread("Imagens Escolhidas/teste2.jpg")
 
-resultado = DeepFace.verify(imagem1, imagem2)
-print('Resultado: ', resultado)
+#face1 = DeepFace.detectFace(imagem1)
+#face2 = DeepFace.detectFace(imagem2)
 
-cv2.imshow("Imagem 1", imagem1)
-cv2.imshow("Imagem 2", imagem2)
+#cv2.imshow("Face Detectada 1", face1)
+#cv2.imshow("Face Detectada 2", face2)
 
-cv2.waitKey(0)
+#resultado = DeepFace.verify(imagem1, imagem2)
+
+#cv2.imshow("Imagem 1 Original", imagem1)
+#cv2.imshow("Imagem 2 Original", imagem2)
+
+#print('Resultado: ', resultado)
+
+a = 'python'
+b = [1, 2, 3]
+
+atup = (a, b)
+
+print(atup[1][1:])
+
+
+
+
