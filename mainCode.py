6d940d79cd05e5a2d4ed5c67cbda2925d6c98ebd
ED CARLOS DA SILVA PEREIRA <63868640+Goghed@users.noreply.github.com>
2022-04-11 18:49:26 -0300

    update 1.0.4

--- mainCode.py
+++ mainCode.py
@@ -12,6 +12,11 @@ def detectar(origem):
     acertou = 0
     errou = 0
     cont = 0
+    tempMax = 0
+    tempMin = 0
+    tempMedia = 0
+    tempTotal = 0
+    tempos = []
 
     # pegar o tempo inicial da execução da função
     tempoInicial = time.time()
@@ -21,6 +26,13 @@ def detectar(origem):
 
     # para cada arquivo verificar a sua extensão, se for .gif converter para .jpg
     for arquivo in diretorio:
+        
+        if ".jpeg" in arquivo:
+            # pega o nome do arquivo em .jpeg, .png ou .bmp e retira a extensão. exemplo: teste.gif para teste sem a extensão .gif
+            nomeArquivo = os.path.splitext(arquivo)[0]
+            # converte o arquivo .jpeg para .jpg ja passando o nome inicial do arquivo e adicionando a extensão .jpg
+            imagem = Image.open(arquivo).convert('RGB').save(nomeArquivo+'.'+'jpg')
+
         if ".gif" in arquivo:
             # pega o nome do arquivo em .gif, .png ou .bmp e retira a extensão. exemplo: teste.gif para teste sem a extensão .gif
             nomeArquivo = os.path.splitext(arquivo)[0]
@@ -28,6 +40,9 @@ def detectar(origem):
             imagem = Image.open(arquivo).convert('RGB').save(nomeArquivo+'.'+'jpg')
 
         if ".jpg" in arquivo:
+
+            tempProcessamentoInicial = time.time()/100          
+
             # se ja for um arquivo .jpg, então ele será passado para a função de reconhecimento
             imagemJpg = cv2.imread(arquivo)
             # chama a função de reconhecimento
@@ -38,7 +53,7 @@ def detectar(origem):
                 # se o conteúdo dentro do array entregue a maioria tiver dados acima de 0, então a face foi encontrada
                 if item.any() > 0:
                     cont += 1
-                    #print('Cont {}'.format(cont))
+                    
                 # se o conteúdo dentro do array entregue a maioria tiver dados abaixo de 0, então a face não foi encontrada
                 else:
                     cont = 0
@@ -51,11 +66,43 @@ def detectar(origem):
                 nomeArquivo = os.path.splitext(arquivo)[0]
                 cv2.imwrite(f"Faces Detectadas/{nomeArquivo}.jpg", detected_face[:, :, ::-1])
                 acertou += 1
-                print('Acertou {} '.format(acertou))
+                
             # se não, então a face não foi encontrada e irá contar quantidade de imagens erradas
             else:
                 errou += 1
-                print('Errou {} '.format(errou))
+
+            tempProcessamentoFinal = time.time()/100
+
+            # calcula o tempo de processamento da imagem
+            tempProcessamento = tempProcessamentoFinal - tempProcessamentoInicial
+            # salva o tempo de processamento da imagem em um array
+            tempos.append(tempProcessamento)            
+
+            # para cada tempo em tempos verificar o maior e o menor
+            for tempo in tempos:
+                if tempo > tempMax:
+                    tempMax = tempo
+                if tempo < tempMax:
+                    tempMin = tempo            
+                tempTotal += tempo           
+            
+            # calcula a média dos tempos de processamento das imagens
+            tempMedia = tempTotal / len(tempos)   
+            
+    # imprime o tempo total de processamento das imagens
+    print("Tempo total de processamento das imagens: {0:.3f}".format(tempTotal))
+    # imprime o tempo máximo de processamento das imagens
+    print("Tempo máximo de processamento das imagens: {0:.3f}".format(tempMax))
+    # imprime o tempo mínimo de processamento das imagens
+    print("Tempo mínimo de processamento das imagens: {0:.3f}".format(tempMin))
+    # imprime a média dos tempos de processamento das imagens
+    print("Tempo médio de processamento das imagens: {0:.3f}".format(tempMedia))
+    # imprime a quantidade de imagens que foram processadas
+    print(f"Quantidade de imagens processadas: {cont}")
+    # imprime a quantidade de imagens que foram encontradas
+    print(f"Quantidade de imagens encontradas: {acertou}")
+    # imprime a quantidade de imagens que não foram encontradas
+    print(f"Quantidade de imagens não encontradas: {errou}")           
 
     # tempo final da função
     tempoFinal = time.time()
@@ -70,35 +117,25 @@ def detectar(origem):
     messagebox.showinfo('Resultado', 'Detectou {0} faces de um total de {1} imagens armazenadas em {2} segundos. Precisão de {3:.2f}% de acerto'.format(acertou, totalImagens, segundos, precisao))
 
 
-def analisar(arquivo, diretorio):
-
-    analise = DeepFace.find(arquivo, diretorio)
-    abrirAnalise = cv2.imread(analise)
-    cv2.imshow('Análise da Imagem', abrirAnalise)
-    cv2.waitKey(0)
-
-
+# função principal
 print('Sistema para detecção e verificação de imagens utilizando o DeepFace')
 
 print('1 - Treinamento do Algoritmo')
 print('2 - Verificação da Imagem')
 print('3 - Sair do programa')
 
+# variável de controle
 decisao = int(input('Digite o valor da sua opção: '))
 
+# se a opção for 1, então irá rodar a função de treinamento
 if decisao == 1:
     print('Treinamento do Algoritmo')
+    
     nomeDiretorio = input('Informe o nome do Diretório que contém as imagens: ')
     origem = os.chdir(nomeDiretorio)
     detectar(origem)
 
-elif decisao == 2:
-    #print('Verificação de Imagem')
-    #nomeArquivo = input('Informe o caminho da imagem: ')
-    #nomeDiretorio = input('Informe o nome do Diretório que contém as imagens: ')
-    arquivo = os.chdir('Img Escolhidas/morena.jpg')
-    diretorio = os.chdir('Banco Imagens/Faces Detectadas')
-    analisar(arquivo, diretorio)
+
 
 
 
--- teste.py
+++ teste.py
@@ -1,11 +1,14 @@
 from deepface import DeepFace
+import cv2
+import pandas as pd
 import os
 
 
-foto = os.chdir(r'C:\Users\gog_e\Documents\GitHub\DeepFace-Face-Recognition\Img Escolhidas\morena.jpg')
-diretorio = os.chdir(r'C:\Users\gog_e\Documents\GitHub\DeepFace-Face-Recognition\Banco Imagens\Faces Detectadas')
-face = DeepFace.find(img_path=foto, db_path=diretorio)
-face.head()
+foto = 'subject01.glasses.jpg'
+diretorio = 'Banco Imagens/Faces Detectadas'
+face = DeepFace.find(img_path=foto, db_path=diretorio, enforce_detection=False)
+print(face.head())
+
 
 
 
